6c37b8720f75bd09e93dfffb69c3bdf57454ebe6
Noah Nachtigall <noah.nachtigall2011@icloud.com>
2026-03-15 02:48:00 +0100

    valve OPEN/CLOSE concept

--- gewaechshaussensorV1/gewaechshaussensorV1.ino
+++ gewaechshaussensorV1/gewaechshaussensorV1.ino
@@ -6,6 +6,9 @@ rgb_lcd lcd;
 int pirPin = 2;
 
 AccelStepper stepper(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
+int valveOpenSteps = 512;   // ~99° bei 200 Steps/Umdrehung
+bool valveOpen = false;
+
 
 int button = 3;   //attach a button to digital pin 3
 int LED = 7;      //attach an LED to digital pin 7
@@ -14,6 +17,7 @@ int lightsensor = 3;
 int points = 0;
 int brightness = 100;
 int lastvalue = 0;
+int potentiometer = 7;
 unsigned long startTime;
 unsigned long currentTime;
 const unsigned long period = 3000;
@@ -23,23 +27,26 @@ const unsigned long lightPeriod = 10000;
 float currentTempValue = 0;
 int addr = 0;
 
+
 void setup()
 {
     pinMode(pirPin, INPUT);
     Serial.begin(9600);
     pinMode(tempsensor, INPUT);
-    pinMode(button, INPUT);  //define button an INPUT device
+    pinMode(button, INPUT_PULLUP);  //define button an INPUT device
     pinMode(LED, OUTPUT); 
+    pinMode(potentiometer, INPUT);
+    
     lcd.begin(16, 2);
-    lcd.setRGB(0, 0, brightness);
+    lcd.setRGB(255, 255, 255);
     lcd.setCursor(0, 0); //define LED an OUTPUT device
     lcd.print("Temp.:");
     lcd.setCursor(10, 0);
     lcd.print("Light:");
     startTime = millis();
     startLight = millis();
-    stepper.setMaxSpeed(2500);
-    stepper.setSpeed(400);
+    stepper.setMaxSpeed(500);
+    stepper.setAcceleration(200);
     
     
 }
@@ -50,6 +57,8 @@ void loop()
     
     int value = analogRead(tempsensor);
     int buttonState = digitalRead(button); //read the status of the button
+    int raw_brightness = digitalRead(potentiometer);
+    int brightness = (raw_brightness /4);
     
     int raw_light = analogRead(lightsensor); // read the raw value from light_sensor pin (A3)
     int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
@@ -57,17 +66,31 @@ void loop()
     currentTime = millis();
     currentLight = millis();
     currentTempValue = (value / 1.8 - 32) / 10;
+    stepper.run();
 
-    if (buttonState == 1){
+    if (light <= 20){
       if (currentLight - startLight >= lightPeriod){
-        lcd.setRGB(0, 0, 255);
+        lcd.setRGB(100, 100, 100);
         startLight = currentLight;
       }
-      
     }
-    else{
-      lcd.setRGB(0, 0, 50);
+     else {
+      lcd.setRGB(255, 255, 255);
     }
+    
+    
+    if (digitalRead(button) == LOW) {   // Knopf gedrückt
+    delay(200);                          // kleines Entprellen
+
+    valveOpen = !valveOpen;              // Zustand wechseln
+
+    if (valveOpen) {
+      stepper.moveTo(valveOpenSteps);    // Ventil auf
+    } else {
+      stepper.moveTo(0);                 // Ventil zu
+    }
+    
+    } 
      
     if (currentTime - startTime >= period){
       
@@ -75,6 +98,21 @@ void loop()
       lcd.print(currentTempValue);
       lcd.setCursor(11, 1);
       lcd.print(light);
+      Serial.println("");
+      Serial.println("");
+      Serial.println("");
+      Serial.println("Temperture:");
+      Serial.println(currentTempValue);
+      Serial.println("----");
+      Serial.println("Light:");
+      Serial.println(light);
+      Serial.println("----");
+      if (valveOpen == true){
+         Serial.println("Valve Open");
+      }
+      if (valveOpen == false){
+        Serial.println("Valve Closed");
+      }
       EEPROM.write(addr, currentTempValue);
       addr = addr + 1;
       if (addr == EEPROM.length()){
@@ -86,21 +124,18 @@ void loop()
     
 
     
-    
-    
-    
-
-    
-
-    
-    
-    
-    
-      
-
-    
-    //if(buttonState == 1)
-    //digitalWrite(LED,1);
-    //else
-    //digitalWrite(LED,0);
+   
 }
+
+//Days since Projectstart (08.03.2026)(free days inclusive):  8
+//Finish Date: X
+
+/*  ----Problems to fix:----
+   - potentiometer to adjust brightness not working(idk why) 
+   - eeprom memory fix(saved memory not readable)
+   */
+
+/*  ------IDEAS--------
+    -data send to computer per wifi(concept in serial monitor)*/
+
+
